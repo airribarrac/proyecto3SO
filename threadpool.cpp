06afa81d4bfa476ee73cbd4f4287f30271f46c84
Irri <irri96@live.cl>
2018-06-27 23:08:38 -0400

    poto

--- threadpool.cpp
+++ threadpool.cpp
@@ -1,5 +1,5 @@
 #include <functional>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 class ThreadPool{
@@ -9,16 +9,18 @@ private:
 	condition_variable cv;
 	mutex mtx,qmtx;
 	queue<function<void(void)>> tareas;
-	auto waitforwork(){
+	void waitforwork(){
+		unique_lock<mutex> lock(mtx);
 		while(1){
-			cv.wait(mtx);
+			cv.wait(lock);	//hace tuto
 			qmtx.lock();
 			//saco tarea;
 			auto tarea = tareas.front();
 			tareas.pop();
 			qmtx.unlock();
-
 			disponibles--;
+			//HACER TAREA
+			tarea();
 			disponibles++;
 		}
 	}
@@ -28,7 +30,7 @@ public:
 		size=_size;
 		disponibles=size;
 		for(int i=0;i<size;i++){
-			thread t(waitforwork);
+			thread t([this]{waitforwork();});
 		}
 	}
 	template<
@@ -40,7 +42,12 @@ public:
 		argumentos && ...args) -> std::packaged_task<MRtrn(void)> {
 		auto aux = std::bind(std::forward<funcion>(func),
 		std::forward<argumentos>(args)...	);
+		tareas.push(aux);
 		return std::packaged_task<MRtrn(void)>(aux);
 	}
 };
 
+
+int main(){
+
+}
